823e2717311f1118670055d5dc246ae133e597d2
dotoooo22 <dt2gg2@gmail.com>
2022-06-06 02:27:51 +0900

    선택, 삽입, 버블정렬

---
+++ hw12-sorting-hashing.c
@@ -0,0 +1,238 @@
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define MAX_ARRAY_SIZE 13	//배열최대사이즈
+#define MAX_HASH_TABLE_SIZE MAX_ARRAY_SIZE	//해쉬테이블 최대사이즈
+
+int initialize(int **a);		//초기화
+int freeArray(int *a);		//메모리해제
+void printArray(int *a);	//프린트
+
+int selectionSort(int *a);		//선택정렬
+int insertionSort(int *a);		//삽입정렬
+int bubbleSort(int *a);		//버블정렬
+int shellSort(int *a);		//셀정렬
+
+int quickSort(int *a, int n);	//퀵정렬
+
+
+
+int hashCode(int key);		//해쉬함수
+
+
+int hashing(int *a, int **ht);		//해쉬테이블 생성
+
+int search(int *ht, int key);		//키찾고 인덱스 리턴
+
+
+int main()
+{
+	char command;
+	int *array = NULL;
+	int *hashtable = NULL;
+	int key = -1;
+	int index = -1;
+
+	srand(time(NULL));
+
+	do{
+		printf("----------------------------------------------------------------\n");
+		printf("                        Sorting & Hashing                       \n");
+		printf("----------------------------------------------------------------\n");
+		printf(" Initialize       = z           Quit             = q\n");
+		printf(" Selection Sort   = s           Insertion Sort   = i\n");
+		printf(" Bubble Sort      = b           Shell Sort       = l\n");
+		printf(" Quick Sort       = k           Print Array      = p\n");
+		printf(" Hashing          = h           Search(for Hash) = e\n");
+		printf("----------------------------------------------------------------\n");
+
+		printf("Command = ");
+		scanf(" %c", &command);
+
+		switch(command) {
+		case 'z': case 'Z':
+			initialize(&array);
+			break;
+		case 'q': case 'Q':
+			freeArray(array);
+			break;
+		case 's': case 'S':
+			selectionSort(array);
+			break;
+		case 'i': case 'I':
+			insertionSort(array);
+			break;
+		case 'b': case 'B':
+			bubbleSort(array);
+			break;
+		case 'l': case 'L':
+			shellSort(array);
+			break;
+		case 'k': case 'K':
+			printf("Quick Sort: \n");
+			printf("----------------------------------------------------------------\n");
+			printArray(array);
+			quickSort(array, MAX_ARRAY_SIZE);
+			printf("----------------------------------------------------------------\n");
+			printArray(array);
+
+			break;
+
+		case 'h': case 'H':
+			printf("Hashing: \n");
+			printf("----------------------------------------------------------------\n");
+			printArray(array);
+			hashing(array, &hashtable);
+			printArray(hashtable);
+			break;
+
+		case 'e': case 'E':
+			printf("Your Key = ");
+			scanf("%d", &key);
+			printArray(hashtable);
+			index = search(hashtable, key);
+			printf("key = %d, index = %d,  hashtable[%d] = %d\n", key, index, index, hashtable[index]);
+			break;
+
+		case 'p': case 'P':
+			printArray(array);
+			break;
+		default:
+			printf("\n       >>>>>   Concentration!!   <<<<<     \n");
+			break;
+		}
+
+	}while(command != 'q' && command != 'Q');
+
+	return 1;
+}
+
+int initialize(int** a)
+{
+	int *temp = NULL;
+
+	//배열 비어있으면 메모리할당
+	if(*a == NULL) {
+		temp = (int*)malloc(sizeof(int) * MAX_ARRAY_SIZE);
+		*a = temp;  
+	} else
+		temp = *a;
+
+	//랜덤값 넣음
+	for(int i = 0; i < MAX_ARRAY_SIZE; i++)
+		temp[i] = rand() % MAX_ARRAY_SIZE;
+
+	return 0;
+}
+
+int freeArray(int *a)
+{
+	if(a != NULL)	//메모리해제
+		free(a);
+	return 0;
+}
+
+void printArray(int *a)
+{
+	if (a == NULL) {		//없음
+		printf("nothing to print.\n");
+		return;
+	}
+	for(int i = 0; i < MAX_ARRAY_SIZE; i++)
+		printf("a[%02d] ", i);
+	printf("\n");
+	for(int i = 0; i < MAX_ARRAY_SIZE; i++)
+		printf("%5d ", a[i]);
+	printf("\n");
+}
+
+
+int selectionSort(int *a)
+{
+	int min;
+	int minindex;
+	int i, j;
+
+	printf("Selection Sort: \n");
+	printf("----------------------------------------------------------------\n");
+
+	printArray(a);
+
+	for (i = 0; i < MAX_ARRAY_SIZE; i++)
+	{
+		minindex = i;
+		min = a[i];
+		for(j = i+1; j < MAX_ARRAY_SIZE; j++)		//i번째부터 제일 작은 거 찾아서
+		{
+			if (min > a[j])
+			{
+				min = a[j];
+				minindex = j;
+			}
+		}
+		a[minindex] = a[i];		//i번째에 작은 걸 넣기
+		a[i] = min;
+	}
+
+	printf("----------------------------------------------------------------\n");
+	printArray(a);
+	return 0;
+}
+
+int insertionSort(int *a)
+{
+	int i, j, t;
+
+	printf("Insertion Sort: \n");
+	printf("----------------------------------------------------------------\n");
+
+	printArray(a);
+
+	for(i = 1; i < MAX_ARRAY_SIZE; i++)
+	{
+		t = a[i];
+		j = i;
+		while (a[j-1] > t && j > 0)		//맞는 위치 찾아 넣기
+		{
+			a[j] = a[j-1];
+			j--;
+		}
+		a[j] = t;
+	}
+
+	printf("----------------------------------------------------------------\n");
+	printArray(a);
+
+	return 0;
+}
+
+int bubbleSort(int *a)
+{
+	int i, j, t;
+
+	printf("Bubble Sort: \n");
+	printf("----------------------------------------------------------------\n");
+
+	printArray(a);
+
+	for(i = 0; i < MAX_ARRAY_SIZE; i++)
+	{
+		for (j = 0; j < MAX_ARRAY_SIZE; j++)
+		{
+			if (a[j-1] > a[j])		//이전게 더 크면 바꾸기
+			{
+				t = a[j-1];
+				a[j-1] = a[j];
+				a[j] = t;
+			}
+		}
+	}
+
+	printf("----------------------------------------------------------------\n");
+	printArray(a);
+
+	return 0;
+}
